1c9592fa06a9c4f83daf1f1d3671ca23d69b1d59
Adam Ferreira <adam.ferreira.dc@gmail.com>
2013-12-31 19:31:41 +0100

    Le client a maintenant des méthodes à completer (client et server compilent)

--- Code/netez/Client.cc
+++ Code/netez/Client.cc
@@ -25,13 +25,75 @@ namespace bomberman
 
     session_on_client(socket& io): session_base(io), state(STATE_STARTING)
     {
-     // sig_begin          .connect(EZMETHOD(this,on_begin));
+      sig_begin.connect(EZMETHOD(this,on_begin));
+      proto.Err.sig_recv.connect(EZMETHOD(this,do_err));
+      proto.Go.sig_recv.connect(EZMETHOD(this,do_go));
+      proto.Board.sig_recv.connect(EZMETHOD(this,do_board));
+      proto.Won.sig_recv.connect(EZMETHOD(this,do_won));
+      proto.Moved.sig_recv.connect(EZMETHOD(this,do_moved));
+      proto.Explosion.sig_recv.connect(EZMETHOD(this,do_explosion));
+      proto.Bomb.sig_recv.connect(EZMETHOD(this,do_bomb));
+      proto.Die.sig_recv.connect(EZMETHOD(this,do_die));
+      proto.BlockBreaked.sig_recv.connect(EZMETHOD(this,do_blockbreaked));
     }
 
-    //void on_begin();
+    void on_begin();
+    void do_err(string);
+    void do_go();
+    void do_board();
+    void do_won(int);
+    void do_moved(int,int,int);
+    void do_explosion(int,int);
+    void do_bomb(int,int);
+    void do_die(int);
+    void do_blockbreaked(int,int);
 };
 
- // void session_on_client::on_begin();
+
+
+  void session_on_client::on_begin(){
+
+  }
+
+  void session_on_client::do_err(string msg){
+
+  }
+
+  void session_on_client::do_go(){
+
+  }
+
+
+  void session_on_client::do_board(){
+
+  }
+
+  void session_on_client::do_won(int id){
+
+  }
+
+
+  void session_on_client::do_moved(int posx,int posy,int id){
+
+  }
+
+
+  void session_on_client::do_explosion(int posx,int posy){
+
+  }
+
+  void session_on_client::do_bomb(int posx,int posy){
+
+  }
+
+
+  void session_on_client::do_die(int id){
+
+  }
+
+  void session_on_client::do_blockbreaked(int posx,int posy){
+
+  }
 
 
 }
